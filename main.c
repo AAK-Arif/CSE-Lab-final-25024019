ea56ee475ae3ae3159875560a15831cd1201b6fa
AAK-Arif <90594165+AAK-Arif@users.noreply.github.com>
2025-11-25 13:31:56 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,312 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+float points(float gp, float cv);
+
+int main()
+{   int AVE4201,AVE4202,CSE4201,CSE4202,CHEM4201,CHEM4202,MATH4203,HUM4203,ASE4202;
+    float ave4201gp,ave4202gp,cse4201gp,cse4202,chem4201gp,cse4202gp,math4203gp,hum4203gp,ase4202gp;
+    printf("Enter score for AVE 4201: ");
+    scanf("%d",&AVE4201);
+    printf("Enter score for CSE 4201: ");
+    scanf("%d",&CSE4201);
+    printf("Enter score for HUM 4203: ");
+    scanf("%d",&HUM4203);
+    printf("Enter score for MATH 4203: ");
+    scanf("%d",&MATH4203);
+    printf("Enter score for CHEM 4201: ");
+    scanf("%d",&CHEM4201);
+    printf("Enter score for CSE 4202: ");
+    scanf("%d",&CSE4202);
+    printf("Enter score for AVE 4202: ");
+    scanf("%d",&AVE4202);
+    printf("Enter score for CHEM 4202: ");
+    scanf("%d",&CHEM4202);
+    printf("Enter score for ASE 4202: ");
+    scanf("%d",&ASE4202);
+    if (AVE4201>=80)
+    {
+        float ave4201gp=4*3;
+        printf("ave4201grade=%f",ave4201gp);
+    }
+
+    else if(80>AVE4201>=75)
+    {
+        float ave4201gp=3.75*3;
+        printf("ave4201grade=%f",ave4201gp);
+    }
+    else if(75>AVE4201>=70)
+
+
+    {
+        float ave4201gp=3.50*3;
+        printf("ave4201grade=%f",ave4201gp);
+    }
+    else if(70>AVE4201>=65)
+    {
+        float ave4201gp=3.25*3;
+        printf("ave4201grade=%f",ave4201gp);
+    }
+    else if(65>AVE4201>=60)
+    {
+        float ave4201gp=3.00*3;
+        printf("ave4201grade=%f",ave4201gp);
+    }
+    else if(60>AVE4201>=55)
+    {
+        float ave4201gp=2.75*3;
+        printf("ave4201grade=%f",ave4201gp);
+    }
+    else if(55>AVE4201>=50)
+    {
+        float ave4201gp=2.50*3;
+        printf("ave4201grade=%f",ave4201gp);
+    }
+    else if(50>AVE4201>=45)
+    {
+        float ave4201gp=2.25*3;
+        printf("ave4201grade=%f",ave4201gp);
+    }
+    else if (45>AVE4201>=40)
+    {
+        float ave4201gp=2.00*3.00;
+        printf("ave4201grade=%f",ave4201gp);
+    }
+    else if (AVE4201<40)
+    {
+        float ave4201gp=0.00*3.00;
+        printf("ave4201grade=%f",ave4201gp);
+
+    }
+     if (CSE4201>=80)
+    {
+        float cse4201gp=4*3;
+        printf("cse4201grade=%f",cse4201gp);
+    }
+
+    else if(80>CSE4201>=75)
+    {
+        float cse4201gp=3.75*3;
+        printf("cse4201grade=%f",cse4201gp);
+    }
+    else if(75>CSE4201>=70)
+
+
+    {
+        float cse4201gp=3.50*3;
+        printf("cse4201grade=%f",cse4201gp);
+    }
+    else if(70>CSE4201>=65)
+    {
+        float cse4201gp=3.25*3;
+        printf("cse4201grade=%f",cse4201gp);
+    }
+    else if(65>CSE4201>=60)
+    {
+        float cse4201gp=3.00*3;
+        printf("cse4201grade=%f",cse4201gp);
+    }
+    else if(60>CSE4201>=55)
+    {
+        float cse4201gp=2.75*3;
+        printf("cse4201grade=%f",cse4201gp);
+    }
+    else if(55>CSE4201>=50)
+    {
+        float cse4201gp=2.50*3;
+        printf("cse4201grade=%f",cse4201gp);
+    }
+    else if(50>CSE4201>=45)
+    {
+        float cse4201gp=2.25*3;
+        printf("cse4201grade=%f",cse4201gp);
+    }
+    else if (45>CSE4201>=40)
+    {
+        float cse4201gp=2.00*3.00;
+        printf("cse4201grade=%f",cse4201gp);
+    }
+    else if (CSE4201<40)
+    {
+        float cse4201gp=0.00*3.00;
+        printf("cse4201grade=%f",cse4201gp);
+
+    }
+     if (CHEM4201>=80)
+    {
+        float chem4201gp=4*3;
+        printf("chem4201grade=%f",chem4201gp);
+    }
+
+    else if(80>CHEM4201>=75)
+    {
+        float chem4201gp=3.75*3;
+        printf("chem4201grade=%f",chem4201gp);
+    }
+    else if(75>CHEM4201>=70)
+
+
+    {
+        float chem4201gp=3.50*3;
+        printf("chem4201grade=%f",chem4201gp);
+    }
+    else if(70>CHEM4201>=65)
+    {
+        float chem4201gp=3.25*3;
+        printf("chem4201grade=%f",chem4201gp);
+    }
+    else if(65>CHEM4201>=60)
+    {
+        float chem4201gp=3.00*3;
+        printf("chem4201grade=%f",chem4201gp);
+    }
+    else if(60>CHEM4201>=55)
+    {
+        float chem4201gp=2.75*3;
+        printf("chem4201grade=%f",chem4201gp);
+    }
+    else if(55>CHEM4201>=50)
+    {
+        float chem4201gp=2.50*3;
+        printf("chem4201grade=%f",chem4201gp);
+    }
+    else if(50>CHEM4201>=45)
+    {
+        float chem4201gp=2.25*3;
+        printf("chem4201grade=%f",chem4201gp);
+    }
+    else if (45>CHEM4201>=40)
+    {
+        float chem4201gp=2.00*3.00;
+        printf("chem4201grade=%f",chem4201gp);
+    }
+    else if (CHEM4201<40)
+    {
+        float chem4201gp=0.00*3.00;
+        printf("chem4201grade=%f",chem4201gp);
+
+    }
+        if (MATH4203>=80)
+    {
+        float math4203gp=4*3;
+        printf("math4203grade=%f",math4203gp);
+    }
+
+    else if(80>MATH4203>=75)
+    {
+        float math4203gp=3.75*3;
+        printf("math4203grade=%f",math4203gp);
+    }
+    else if(75>MATH4203>=70)
+
+
+    {
+        float math4203gp=3.50*3;
+        printf("math4203grade=%f",math4203gp);
+    }
+    else if(70>MATH4203>=65)
+    {
+        float math4203gp=3.25*3;
+        printf("math4203grade=%f",math4203gp);
+    }
+    else if(65>MATH4203>=60)
+    {
+        float math4203gp=3.00*3;
+        printf("math4203grade=%f",math4203gp);
+    }
+    else if(60>MATH4203>=55)
+    {
+        float math4203gp=2.75*3;
+        printf("math4203grade=%f",math4203gp);
+    }
+    else if(55>MATH4203>=50)
+    {
+        float math4203gp=2.50*3;
+        printf("math4203grade=%f",math4203gp);
+    }
+    else if(50>MATH4203>=45)
+    {
+        float math4203gp=2.25*3;
+        printf("math4203grade=%f",math4203gp);
+    }
+    else if (45>MATH4203>=40)
+    {
+        float math4203gp=2.00*3.00;
+        printf("math4203grade=%f",math4203gp);
+    }
+    else if (MATH4203<40)
+    {
+        float math4203gp=0.00*3.00;
+        printf("math4203grade=%f",math4203gp);
+
+    }
+      if (HUM4203>=80)
+    {
+        float hum4203gp=4*3;
+        printf("hum4203grade=%f",hum4203gp);
+    }
+
+    else if(80>HUM4203>=75)
+    {
+        float hum4203gp=3.75*3;
+        printf("hum4203grade=%f",hum4203gp);
+    }
+    else if(75>HUM4203>=70)
+
+
+    {
+        float hum4203gp=3.50*3;
+        printf("hum4203grade=%f",hum4203gp);
+    }
+    else if(70>HUM4203>=65)
+    {
+        float hum4203gp=3.25*3;
+        printf("hum4203grade=%f",hum4203gp);
+    }
+    else if(65>HUM4203>=60)
+    {
+        float hum4203gp=3.00*3;
+        printf("hum4203grade=%f",hum4203gp);
+    }
+    else if(60>HUM4203>=55)
+    {
+        float hum4203gp=2.75*3;
+        printf("hum4203grade=%f",hum4203gp);
+    }
+    else if(55>HUM4203>=50)
+    {
+        float hum4203gp=2.50*3;
+        printf("hum4203grade=%f",hum4203gp);
+    }
+    else if(50>HUM4203>=45)
+    {
+        float hum4203gp=2.25*3;
+        printf("hum4203grade=%f",hum4203gp);
+    }
+    else if (45>HUM4203>=40)
+    {
+        float hum4203gp=2.00*3.00;
+        printf("hum4203grade=%f",hum4203gp);
+    }
+    else if (HUM4203<40)
+    {
+        float hum4203gp=0.00*3.00;
+        printf("hum4203grade=%f",hum4203gp);
+
+    }
+
+
+
+    printf("Total Credits: 19.50");
+
+
+    //printf("Total Grade Points Earned: %2f",points);
+
+    return 0;
+}
+ //float points(float gp,float cv);
+ //points=(gp*cv) ;
+//result points;
+
